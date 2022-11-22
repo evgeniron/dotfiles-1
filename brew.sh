fdad422f8e192d22b3539b86742301c4711b39ee
Evgeni R. Shtrakh <5058357+evgeniron@user.noreply.github.com>
2022-11-22 11:14:22 +0200

    custom changes

--- brew.sh
+++ brew.sh
@@ -38,64 +38,44 @@ brew install wget --with-iri
 # Install GnuPG to enable PGP-signing commits.
 brew install gnupg
 
+# Taps
+tap 'homebrew/bundle'
+tap 'homebrew/core'
+tap 'homebrew/services'
+tap 'bufbuild/buf'
+
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install screen
-brew install php
-brew install gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+brew install neovim --with-override-system-vi
+brew install ripgrep
+brew install tmux
+brew install fd
+brew install fzf
+brew install go
+brew install kubectl
+brew install zoxide
 
 # Install other useful binaries.
 brew install ack
-#brew install exiv2
 brew install git
 brew install git-lfs
-brew install gs
-brew install imagemagick --with-webp
 brew install lua
-brew install lynx
 brew install p7zip
-brew install pigz
-brew install pv
 brew install rename
 brew install rlwrap
 brew install ssh-copy-id
 brew install tree
-brew install vbindiff
-brew install zopfli
+brew install docker
+brew install gh
+brew install kubectl
+brew install protoc-gen-go-grpc
+brew install --cask alacritty
+brew install --cask google-cloud-sdk
+brew install --cask maccy
+brew install --cask slack
+brew install --cask whatsapp
+brew install --cask zoom
+brew install --cask beekeeper-studio
+brew install --cask obsidian
 
 # Remove outdated versions from the cellar.
 brew cleanup
